77d12446966a8bb2b03808aaf740f233a81a3773
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-07-03 23:55:24 +0500

    Add font family poppins

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
-
 import {
   Container,
   Span,
